4d416edecd0b0bdc35109ec8c6c62560986169fb
DennisBilowodskyj <d.bilowodskyi@gmail.com>
2024-04-17 10:13:24 +0200

    input

--- src/app/contact-section/contact-section.component.ts
+++ src/app/contact-section/contact-section.component.ts
@@ -1,7 +1,8 @@
-import { CommonModule } from '@angular/common';
-import { Component, viewChild } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { Component, inject, } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import {NgForm} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 import {
   AbstractControl,
   FormBuilder,
@@ -18,76 +19,90 @@ import Validation from './validation';
   templateUrl: './contact-section.component.html',
   styleUrl: './contact-section.component.scss'
 })
+
 export class ContactSectionComponent {
-message: any;
-isChecked: boolean = false;
-toggleCheck() {
-  this.isChecked = !this.isChecked;
-}
-requiredMessage() {
-throw new Error('Method not implemented.');
-}
-  form: FormGroup = new FormGroup({
-    fullname: new FormControl(''),
-    username: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
-    acceptTerms: new FormControl(false),
-  });
-  submitted = false;
 
-  constructor(private formBuilder: FormBuilder) {}
+http = inject(HttpClient);  
 
-  ngOnInit(): void {
-    this.form = this.formBuilder.group(
-      {
-        fullname: ['', Validators.required],
-        username: [
-          '',
-          [
-            Validators.required,
-            Validators.minLength(6),
-            Validators.maxLength(20),
-          ],
-        ],
-        email: ['', [Validators.required, Validators.email]],
-        password: [
-          '',
-          [
-            Validators.required,
-            Validators.minLength(6),
-            Validators.maxLength(40),
-          ],
-        ],
-        confirmPassword: ['', Validators.required],
-        acceptTerms: [false, Validators.requiredTrue],
+name: string = '';
+showName: boolean = false;
+showText: any;
+text: any;
+showEmail: boolean = false;
+email: string = '';
+isChecked:boolean = false;
+acceptTerms = new FormControl(false);
+  contactForm: any;
+
+requiredName(){
+  if (!this.name){
+    this.showName = true;
+  } else {
+    this.showName = false;
+  }
+}
+
+requiredEmail() {
+  if (!this.email){
+    this.showEmail = true;
+  } else {
+    this.showEmail = false;
+  }
+}
+
+requiredTextMessage(){
+  if (!this.text){
+    this.showText = true;
+  } else {
+    this.showText = false;
+  }
+}
+
+toggleCheckbox(event: any) {
+  this.isChecked=(event.target.checked);
+}
+
+
+
+
+contactData = {
+  name: "",
+  email: "",
+  message: "",
+}
+
+mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
       },
-      {
-        validators: [Validation.match('password', 'confirmPassword')],
-      }
-    );
-  }
-
-  get f(): { [key: string]: AbstractControl } {
-    return this.form.controls;
-  }
-
-  onSubmit(): void {
-    this.submitted = true;
-
-    if (this.form.invalid) {
-      return;
-    }
-
-    console.log(JSON.stringify(this.form.value, null, 2));
-  }
-
-  onReset(): void {
-    this.submitted = false;
-    this.form.reset();
-  }
+    },
+  };
 
  
-}
 
+onSubmit(ngForm: NgForm) {
+  if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+    this.http.post(this.post.endPoint, this.post.body(this.contactData))
+      .subscribe({
+        next: (response) => {
+
+          ngForm.resetForm();
+        },
+        error: (error) => {
+          console.error(error);
+        },
+        complete: () => console.info('send post complete'),
+      });
+  } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+    ngForm.resetForm();
+  }
+ 
+}
+}
