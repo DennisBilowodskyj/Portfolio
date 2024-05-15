52053b4c533c62df38f1658c0c10e4b2bdc43510
DennisBilowodskyj <d.bilowodskyi@gmail.com>
2024-05-15 17:50:21 +0200

    snack-bar

--- src/app/contact-section/contact-section.component.ts
+++ src/app/contact-section/contact-section.component.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBar } from '@angular/material/snack-bar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -33,16 +33,17 @@ export class ContactSectionComponent {
   isError = false;
   privacyPolicyChecked: any;
   showPrivacyP = false;
-  durationInSeconds: number = 3;
 
   constructor(private _snackBar: MatSnackBar) {}
 
+  @NgModule({
+    providers: [
+      {provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {duration: 3000}}
+    ]
+  })
 
   async sendMail() {
-    //let contactMail = new FormData();
-    //contactMail.append('name', this.myForm?.value.name);
-    //contactMail.append('email', this.myForm?.value.email);
-    //contactMail.append('message', this.myForm?.value.message);
+   
 
     let contactMail = {
       name: this.myForm?.value.name,
@@ -62,26 +63,23 @@ export class ContactSectionComponent {
       if (response.ok) {
         this.emailSent = true;
         this._snackBar.open('Email sent successfully', '', {
-          duration: this.durationInSeconds * 1000,
         });
         this.myForm?.resetForm();
       } else {
         this.isError = true;
         this._snackBar.open('Failed to send email', '', {
-          duration: this.durationInSeconds * 1000,
         });
       }
     } catch (error) {
       this.isError = true;
       this._snackBar.open('An error occurred', '', {
-        duration: this.durationInSeconds * 1000,
       });
     } finally {
       setTimeout(() => {
         this.isSending = false;
         this.emailSent = false;
         this.isError = false;
-      }, 3000);
+      }, );
     }
   }
 
@@ -89,43 +87,4 @@ export class ContactSectionComponent {
     this.showPrivacyP = true;
   }
 
-
-  /*mailTest = false;
-
-  post = {
-    endPoint: 'https://dennis-bilowodskyj.com/sendMail.php',
-    body: (payload: any) => JSON.stringify(payload),
-    options: {
-      headers: {
-        'Content-Type': 'text/plain',
-        responseType: 'text',
-      },
-    },
-  };
-
- 
-
-onSubmit(ngForm: NgForm) {
-  if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
-    this.http.post(this.post.endPoint, this.post.body(this.contactData))
-      .subscribe({
-        next: (response) => {
-
-          ngForm.resetForm();
-        },
-        error: (error) => {
-          console.error(error);
-        },
-        complete: () => console.info('send post complete'),
-      });
-  } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
-
-    ngForm.resetForm();
-  }
- 
-}
-  contactData(contactData: any): any {
-    throw new Error('Method not implemented.');
-  }
-  */
 }
